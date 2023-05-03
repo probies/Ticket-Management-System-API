3d9baa418e584a23392c1b822cbf5c7838d74758
Piyush Jha <jhapiyush06@gmail.com>
2023-05-03 12:48:23 +0530

    ticketing app server setup

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+//app
+const app = express();
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// connect app to database
+mongoose
+.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}) // Adding new mongo url parser
+.then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err));
+
+
+// const ticketRoutes = require('./routes/ticket');
+// app.use('/api/user', ticketRoutes);
+
+
+//port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
